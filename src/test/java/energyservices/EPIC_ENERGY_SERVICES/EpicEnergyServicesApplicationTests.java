2bed711b464ff1c2a50a8e7300ebbd095cfc44b7
Thomas-Galbignani <thomas.lino.galbignani@gmail.com>
2025-10-27 10:21:35 +0100

    initial commit

---
+++ src/test/java/energyservices/EPIC_ENERGY_SERVICES/EpicEnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package energyservices.EPIC_ENERGY_SERVICES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
