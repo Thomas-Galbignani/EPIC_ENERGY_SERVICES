0cc75469c307df91d72b3a5aef3e62c4e0f144bc
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-10-29 17:44:53 +0100

    filtro fatture per cliente

--- src/main/java/energyservices/EPIC_ENERGY_SERVICES/specifications/SpecificationFatture.java
+++ src/main/java/energyservices/EPIC_ENERGY_SERVICES/specifications/SpecificationFatture.java
@@ -1,5 +1,40 @@
 package energyservices.EPIC_ENERGY_SERVICES.specifications;
 
+import energyservices.EPIC_ENERGY_SERVICES.entities.Cliente;
+import energyservices.EPIC_ENERGY_SERVICES.entities.Fattura;
+import energyservices.EPIC_ENERGY_SERVICES.entities.Stato;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class SpecificationFatture {
+public static Specification<Fattura> filtraPerCliente(Cliente cliente){
+    return (root,query,cb) -> {
+        return cliente == null ? null : cb.notEqual(root.get("cliente"),cliente);
+
+    };
 
 }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
