78c8d98bf3e2e05b4e03d8577e8e62f130045c1f
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-10-30 22:41:01 +0100

    aggiunto .cors()

--- src/main/java/energyservices/EPIC_ENERGY_SERVICES/configs/security/SecurityConfig.java
+++ src/main/java/energyservices/EPIC_ENERGY_SERVICES/configs/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package energyservices.EPIC_ENERGY_SERVICES.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,6 +26,7 @@ public class SecurityConfig {
         httpSecurity.csrf(cs -> cs.disable());
         httpSecurity.sessionManagement(se -> se.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll());
+        httpSecurity.cors(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
