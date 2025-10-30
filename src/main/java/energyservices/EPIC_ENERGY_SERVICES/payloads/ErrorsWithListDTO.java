860925d391024de2d44a53b40c52232f0f324f15
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-10-30 10:48:45 +0100

    exceptions handler

---
+++ src/main/java/energyservices/EPIC_ENERGY_SERVICES/payloads/ErrorsWithListDTO.java
@@ -0,0 +1,11 @@
+package energyservices.EPIC_ENERGY_SERVICES.payloads;
+
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ErrorsWithListDTO(String message,
+                                LocalDateTime timestamp,
+                                List<String> errorsList) {
+}
